53c597778fba0b33fcb1e72f29f9dce2158e3088
Andrei Vakar <Andrei.Vakar@itechart-group.com>
2020-03-13 15:42:09 +0300

    Replacing case class Status with Flow

---
+++ itechart-project/src/main/scala/by/itechart/entity/Flow.scala
@@ -0,0 +1,8 @@
+package by.itechart.entity
+
+case class Flow(
+                 flowId: Long,
+                 statusId: String,
+                 status_date: String,
+                 record_id: Long
+                 )
--- itechart-project/src/main/scala/by/itechart/entity/Status.scala
+++
@@ -1,7 +0,0 @@
-package by.itechart.entity
-
-case class Status(
-                 statusId: Long,
-                 statusName: String,
-                 active: Boolean
-                 )
